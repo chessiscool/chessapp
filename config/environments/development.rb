98437620d6c6d8292b4d4198df1bb562962d0281
zkbell91 <z.bell@me.com>
2019-08-17 00:22:01 +0000

    Add Devise gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,7 +30,9 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
+  #Setting up mailer to work with Devise
+  config.action_mailer.default_url_options = { host: 'localhost:3030' }
+
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
