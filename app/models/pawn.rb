81c3b4e6f19aac681979582b73d8b70adc4e3d7d
Sarah Crank <sarahcrank88@gmail.com>
2019-08-27 00:17:56 +0000

    Fixed bugs

--- app/models/pawn.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-class Pawn < ApplicationRecord
-end
--- db/migrate/20190825184638_create_pawns.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# This is the first step in setting up the STI on the pieces.
-# This affects the class "Pawn" which is a child of "Piece".
-class CreatePawns < ActiveRecord::Migration[5.2]
-  def change
-    create_table :pawns, &:timestamps
-  end
-end
