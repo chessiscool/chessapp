ae831ab63433fc03764b6cbddb217cf848f93cb1
Courtney Fink <courtney55@knights.ucf.edu>
2019-09-05 21:12:36 +0000

    more fixes

--- db/schema.rb
+++ db/schema.rb
@@ -48,4 +48,20 @@ ActiveRecord::Schema.define(version: 20_190_827_020_225) do
             name: 'index_pieces_on_x_position_and_y_position_and_player_id'
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "user_name", default: "", null: false
+    t.integer "user_id", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["user_id"], name: "index_users_on_user_id", unique: true
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true
+  end
+
 end
