4273d485df92f9e040fce2a702c665c9f9cf21ff
Sarah Crank <sarahcrank88@gmail.com>
2019-09-15 20:51:56 +0000

    Added a top navbar because it was driving me crazy not having one

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -10,14 +10,20 @@ class PiecesController < ApplicationController
     @piece = Piece.find(params[:id])
   end
 
+  def update
+    @piece = Piece.find(params[:id])
+    @game = @piece.game
+  end
+
   private
 
   def piece_params
-    params.require(:piece).permit(:game_id,
-                                  :user_id,
-                                  :type,
-                                  :x_position,
-                                  :y_position)
+    params.require(:piece).permit(
+      :game_id,
+      :user_id,
+      :type,
+      :x_position,
+      :y_position)
   end
 
   def current_game
