a03af6130f593e587e83fdfe5cfbf897fecd0d00
Courtney Fink <courtney55@knights.ucf.edu>
2019-09-18 19:22:25 +0000

    added king model

--- app/models/king.rb
+++ app/models/king.rb
@@ -4,4 +4,25 @@
 # The King class inherits from the Piece class
 class King < Piece
   belongs_to :piece
+
+  def valid_move?(x_position, y_position)
+    if diagonal_move?(x, y)
+      return true if unoccupied?(x, y)
+      return false if occupied_by_opposing_piece?(x, y) && piece_type == king
+
+    elsif vertical_move?(x, y)
+      return true if unoccupied?(x, y)
+      return false if occupied_by_opposing_piece?(x, y) && piece_type == king
+
+    else horizontal_move?(x, y)
+      return true if unoccupied?(x, y)
+      return false if occupied_by_opposing_piece?(x, y) && piece_type == king
+    end
+  end
+
+  def capture
+
+  end
 end
+
+
